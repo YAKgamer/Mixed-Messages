d54541beb957cb74a9a558b5c2893b7da889e035
test111111 <kahwajiamine10@gmail.com>
2024-09-06 16:26:32 +0300

    v1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Mixed-Messages
+# **Mixed Message**
+
+### What it provides:
+* Mixed messages
+* Endless Entertainment
+* A lotta pleasure
+
+### How is it so good?:
+dont worry about it
+
+### How to use:
+just run it and enjoy!
---
+++ main.js
@@ -0,0 +1,13 @@
+const feeling = ['happy', 'sad', 'annoyed', 'horny', 'needy', 'desperate', 'lustful', 'thirsty'];
+const action = ['talk to your loved one', 'touch grass', 'relieve yourself', 'fuck your S.O', 'watch corn', 'video call your partner', 'take a nap', 'drink some holy water'];
+const location = ['on the toilet seat', 'in the garden', 'on your bed', "on your lover's couch", 'in the kitchen', 'in front of the mirror', 'on the family T.V', 'in prison'];
+
+function generateMessage() {
+    const message = [];
+    for (let i=1; i <= 3; i++) {
+        message.push((Math.floor(Math.random()*7)+1));
+    }
+    return `You are feeling ${feeling[message[0]]}. You should ${action[message[1]]} ${location[message[2]]}`;
+}
+
+console.log(generateMessage());
